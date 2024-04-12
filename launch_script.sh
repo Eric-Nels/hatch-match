09a932ea36fe0cda0733f4769ab067338817009f
Eric Nelson <ernelson12o0@gmail.com>
2024-04-12 17:42:28 -0400

    update launch script

--- launch_script.sh
+++ launch_script.sh
@@ -8,13 +8,22 @@ fi
 echo "Launching frontend..."
 cd hatch-match-frontend
 npm install
-npm install axios
+npm install \
+  @testing-library/jest-dom@^5.17.0 \
+  @testing-library/react@^13.4.0 \
+  @testing-library/user-event@^13.5.0 \
+  axios@^1.6.8 \
+  react@^18.2.0 \
+  react-dom@^18.2.0 \
+  react-scripts@5.0.1 \
+  web-vitals@^2.1.4
 npm start &
 
 sleep 5
 
 echo "Launching backend..."
 cd ../hatch-match-backend
+sudo apt install python3
 pip install flask-cors
 pipenv install
 sleep 5
